7a1a8bb5f6c62c679497638e0fa4fed9e3ef2982
Victor Valenzuela <khancerberus@hotmail.cl>
2024-03-30 11:45:16 -0300

    feat: Add 404 page
    - Add lucide-react for icons

--- src/components/Error404.tsx
+++ src/components/Error404.tsx
@@ -1,10 +1,17 @@
+import { Frown } from 'lucide-react'
+
 export const Error404 = (): JSX.Element => {
   return (
-    <main className="flex justify-center content-center" style={{
-      width: '100vw'
-    }}>
-      <section className="">
-        <h1>Not found</h1>
+    <main className="flex w-screen h-screen justify-center content-center">
+      <section className="flex flex-col justify-center content-center text-center">
+        <Frown size={150} className="w-full my-5" />
+        <h1 className="text-6xl">404</h1>
+        <h2 className="text-3xl">Not found</h2>
+        <div className="flex justify-center">
+          <p className="w-3/4 py-5">
+            Esta página no se ha encontrado! Por favor reintente con otra ruta...
+          </p>
+        </div>
       </section>
     </main>
   )
