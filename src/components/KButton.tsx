d06229d6226dbffa5623ed7942a019aefe0363d8
Victor Valenzuela <khancerberus@hotmail.cl>
2024-03-30 14:54:27 -0300

    misc: Add disabled property to KButton

--- src/components/KButton.tsx
+++ src/components/KButton.tsx
@@ -3,13 +3,18 @@ interface KButtonProps {
   label?: string
   icon?: JSX.Element
   onClick?: () => void
+  disabled?: boolean
 }
 
 export const KButton = (props: KButtonProps): JSX.Element => {
   return (
     <button
-      className={'p-2 bg-cyan-700 hover:bg-cyan-900 transition-colors duration-300 ' + props?.className}
+      className={
+        'p-2 bg-cyan-700 enabled:hover:bg-cyan-900 transition-colors duration-300 disabled:opacity-75 ' +
+        props?.className
+      }
       onClick={props?.onClick}
+      disabled={props?.disabled}
     >
       {props?.icon}
       {props?.label}
